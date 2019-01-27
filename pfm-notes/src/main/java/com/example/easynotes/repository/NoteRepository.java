da5b453e748d1e8d79908624c3178e376abb8d83
Your Name <neha.wdh@gmail.com>
2019-01-27 21:11:12 +0800

    neha.wdh@gmail.com

---
+++ pfm-notes/src/main/java/com/example/easynotes/repository/NoteRepository.java
@@ -0,0 +1,16 @@
+package com.example.easynotes.repository;
+
+import com.example.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 
+ * @author neha
+ *
+ */
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
---
+++ pfm-notes/src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.easynotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * 
+ * @author neha
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
